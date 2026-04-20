9e47e37dbcabfe585db63340156939fbb2779383
Nodo1221 <killforskill2005@gmail.com>
2026-04-20 04:57:56 +0200

    Prettier config

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
-import './globals.css'
+import './globals.css';
 
 import App from './App.vue';
 import router from './router';
